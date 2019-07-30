23e2099a6fc7f557fd759747b08afc693d146e00
Mark Barbone <mark.l.barbone@gmail.com>
2019-07-30 17:17:36 -0400

    CI: unzip NDK with -q

--- ci/install.sh
+++ ci/install.sh
@@ -4,4 +4,4 @@ set -ex
 
 # Download NDK
 wget https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
-unzip android-ndk-r20-linux-x86_64.zip
+unzip -q android-ndk-r20-linux-x86_64.zip
