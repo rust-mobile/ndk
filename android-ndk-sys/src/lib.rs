112619555e74ce938b5b824845c21b8ed7d5d52d
Mark Barbone <mark.l.barbone@gmail.com>
2019-07-31 00:22:41 -0400

    android-ndk-sys: silence warnings about u128 in FFI

--- android-ndk-sys/src/lib.rs
+++ android-ndk-sys/src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// To allow for bindgen's use of u128 in some cases
+#![allow(improper_ctypes)]
 
 include!(concat!(env!("OUT_DIR"), "/ffi.rs"));
