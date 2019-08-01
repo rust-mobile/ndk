676fe913f7dc04da8d66d69e92f4177c471f4e3c
Mark Barbone <mark.l.barbone@gmail.com>
2019-08-01 02:09:41 -0400

    Update CI config

--- ci/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# Download NDK
-wget https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
-unzip -q android-ndk-r20-linux-x86_64.zip android-ndk-r20/sysroot/*
--- ci/script.sh
+++ ci/script.sh
@@ -2,8 +2,15 @@
 
 set -ex
 
-export NDK_HOME=$(pwd)/android-ndk-r20
+# Check formatting
+cargo fmt --all -- --check
 
 # Run tests
 cargo test --package android-ndk-sys --lib --target=x86_64-unknown-linux-gnu
-cargo test --package android-ndk --target=x86_64-unknown-linux-gnu
+
+# The most we can do is make sure it compiles
+cargo check --package android-ndk --target=arm-linux-androideabi
+cargo check --package android-ndk --target=armv7-linux-androideabi
+cargo check --package android-ndk --target=aarch64-linux-android
+cargo check --package android-ndk --target=i686-linux-android
+cargo check --package android-ndk --target=x86_64-linux-android
