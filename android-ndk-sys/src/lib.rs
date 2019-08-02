df60bd5a20f30ca76e4c884cd1a11f349ffd83a4
Mark Barbone <mark.l.barbone@gmail.com>
2019-08-01 21:55:14 -0400

    Fix tests on android-ndk-sys

--- android-ndk-sys/src/lib.rs
+++ android-ndk-sys/src/lib.rs
@@ -13,33 +13,25 @@
 // Test setup lints
 #![cfg_attr(test, allow(dead_code))]
 
+pub mod android_native_app_glue;
+
 #[cfg(all(not(target_os = "android"), not(test), not(feature = "rustdoc")))]
 compile_error!("android-ndk-sys only supports compiling for Android");
 
-#[cfg(any(all(target_os = "android", target_arch = "arm"), feature = "rustdoc"))]
+#[cfg(any(
+    all(any(target_os = "android", test), target_arch = "arm"),
+    feature = "rustdoc"
+))]
 include!("ffi_arm.rs");
 
-#[cfg(all(target_os = "android", target_arch = "armv7"))]
+#[cfg(all(any(target_os = "android", test), target_arch = "armv7"))]
 include!("ffi_armv7.rs");
 
-#[cfg(all(target_os = "android", target_arch = "aarch64"))]
+#[cfg(all(any(target_os = "android", test), target_arch = "aarch64"))]
 include!("ffi_aarch64.rs");
 
-#[cfg(all(target_os = "android", target_arch = "x86"))]
+#[cfg(all(any(target_os = "android", test), target_arch = "x86"))]
 include!("ffi_i686.rs");
 
-#[cfg(all(target_os = "android", target_arch = "x86_64"))]
+#[cfg(all(any(target_os = "android", test), target_arch = "x86_64"))]
 include!("ffi_x86_64.rs");
-
-pub mod android_native_app_glue;
-
-#[cfg(all(test, target_arch = "aarch64"))]
-mod ffi_aarch64;
-#[cfg(all(test, target_arch = "arm"))]
-mod ffi_arm;
-#[cfg(all(test, target_arch = "armv7"))]
-mod ffi_armv7;
-#[cfg(all(test, target_arch = "x86"))]
-mod ffi_i686;
-#[cfg(all(test, target_arch = "x86_64"))]
-mod ffi_x86_64;
