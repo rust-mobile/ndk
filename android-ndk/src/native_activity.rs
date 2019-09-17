3de347728adf2bff2d8dfec7db62da2939eff969
Mark Barbone <mark.l.barbone@gmail.com>
2019-09-16 22:52:43 -0400

    Add functionality to native_activity

--- android-ndk/src/native_activity.rs
+++ android-ndk/src/native_activity.rs
@@ -3,6 +3,7 @@
 //! See also [the NDK
 //! docs](https://developer.android.com/ndk/reference/struct/a-native-activity.html)
 
+//use num_enum::{IntoPrimitive, TryFromPrimitive};
 use std::ffi::CStr;
 use std::os::raw::c_void;
 use std::ptr::NonNull;
@@ -28,7 +29,13 @@ impl NativeActivity {
     pub fn ptr(&self) -> NonNull<ffi::ANativeActivity> {
         self.ptr
     }
+}
 
+/// Methods that relate to fields of the struct itself
+///
+/// The relevant NDK docs can be found
+/// [here.](https://developer.android.com/ndk/reference/struct/a-native-activity)
+impl NativeActivity {
     /// The platform's SDK version code
     pub fn sdk_version(&self) -> i32 {
         unsafe { self.ptr.as_ref().sdkVersion }
@@ -66,7 +73,7 @@ impl NativeActivity {
     /// This processe's `JavaVM` object.
     ///
     /// ```no_run
-    /// #let native_activity: NativeActivity = unimplemented!();
+    ///# let native_activity: NativeActivity = unimplemented!();
     /// let vm = native_activity.vm();
     /// let env = vm.attach_current_thread();
     /// // Do JNI with env ...
@@ -92,3 +99,62 @@ impl NativeActivity {
         CStr::from_ptr(self.ptr.as_ref().obbPath)
     }
 }
+
+/// Methods that relate to `ANativeActivity_*` functions.
+///
+/// The relevant NDK docs can be found
+/// [here.](https://developer.android.com/ndk/reference/group/native-activity)
+impl NativeActivity {
+    /// Sends a destroy event to the activity and stops it.
+    pub fn finish(&self) {
+        unsafe { ffi::ANativeActivity_finish(self.ptr.as_ptr()) }
+    }
+
+    /// Shows the IME (the on-screen keyboard).
+    ///
+    /// If `force` is true, the `SHOW_FORCED` flag is used; otherwise, the `SHOW_IMPLICIT` flag is
+    /// used.  Depending on the value of this flag, the `hide_soft_input` method with behave
+    /// differently.  See [the relevant
+    /// javadoc](https://developer.android.com/reference/android/view/inputmethod/InputMethodManager#constants_2)
+    /// for more information.
+    pub fn show_soft_input(&self, force: bool) {
+        let flag = if force {
+            ffi::ANATIVEACTIVITY_SHOW_SOFT_INPUT_FORCED
+        } else {
+            ffi::ANATIVEACTIVITY_SHOW_SOFT_INPUT_IMPLICIT
+        };
+        unsafe { ffi::ANativeActivity_showSoftInput(self.ptr.as_ptr(), flag) }
+    }
+
+    /// Hides the IME (the on-screen keyboard).
+    ///
+    /// If `not_always` is true, the `HIDE_NOT_ALWAYS` flag is used; otherwise, the
+    /// `HIDE_IMPLICIT_ONLY` flag is used.  Depending on the value of this flag and the way the IME
+    /// was shown, it may or may not be hidden.  See [the relevant
+    /// javadoc](https://developer.android.com/reference/android/view/inputmethod/InputMethodManager#constants_2)
+    /// for more information.
+    pub fn hide_soft_input(&self, not_always: bool) {
+        let flag = if not_always {
+            ffi::ANATIVEACTIVITY_HIDE_SOFT_INPUT_NOT_ALWAYS
+        } else {
+            ffi::ANATIVEACTIVITY_HIDE_SOFT_INPUT_IMPLICIT_ONLY
+        };
+        unsafe { ffi::ANativeActivity_hideSoftInput(self.ptr.as_ptr(), flag) }
+    }
+
+    /*/// Set the window format. Performs the Java `.getWindow().setFormat()`.
+    ///
+    /// See also [the relevant
+    /// javadoc](https://developer.android.com/reference/android/view/Window#setFormat(int))
+    pub unsafe fn set_window_format(&self, format: WindowFormat) {
+        unsafe { ffi::ANativeActivity_setWindowFormat(self.ptr.as_ptr(), format.into()) }
+    }*/
+}
+
+/*#[derive(Debug, Clone, Copy, PartialEq, Eq, TryFromPrimitive, IntoPrimitive)]
+#[repr(u32)]
+pub enum WindowFormat {
+    Rgb565 = ffi::ANativeWindow_LegacyFormat_WINDOW_FORMAT_RGB_565,
+    Rgba8888 = ffi::ANativeWindow_LegacyFormat_WINDOW_FORMAT_RGBA_8888,
+    Rgbx8888 = ffi::ANativeWindow_LegacyFormat_WINDOW_FORMAT_RGBX_8888,
+}*/
