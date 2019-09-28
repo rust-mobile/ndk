b9433729903841139bf3755d94fa95c20d9a2c30
Mark Barbone <mark.l.barbone@gmail.com>
2019-09-28 16:55:05 -0400

    Fix publishing to crates.io in CI

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+cd "$(dirname "$0")/.."
+
 # Check formatting
 cargo fmt --all -- --check
 
@@ -9,12 +11,9 @@ cargo fmt --all -- --check
 cargo test --package android-ndk-sys --lib --target=x86_64-unknown-linux-gnu
 
 # Make sure doc tests compile
-cargo test --manifest-path android-ndk/Cargo.toml --doc --target=x86_64-unknown-linux-gnu --features rustdoc
+CC=cc AR=ar cargo test --manifest-path android-ndk/Cargo.toml --doc --target=x86_64-unknown-linux-gnu --features rustdoc
 
 # Make sure it compiles on each platform
-export CC=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/clang
-export AR=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
-
 cargo check --package android-ndk --target=arm-linux-androideabi
 cargo check --package android-ndk --target=armv7-linux-androideabi
 cargo check --package android-ndk --target=aarch64-linux-android
