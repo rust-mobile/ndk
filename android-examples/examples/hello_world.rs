af677c11f8cfccc2afac6b54c1cb425f21945ee3
David Craven <david@craven.ch>
2020-02-11 22:19:07 +0100

    Add android-examples.

---
+++ android-examples/examples/hello_world.rs
@@ -0,0 +1,19 @@
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub unsafe extern "C" fn ANativeActivity_onCreate(
+    activity: *mut std::os::raw::c_void,
+    saved_state: *mut std::os::raw::c_void,
+    saved_state_size: usize,
+) {
+    std::env::set_var("RUST_BACKTRACE", "1");
+    android_glue::init(
+        activity as _,
+        saved_state as _,
+        saved_state_size as _,
+        app_main,
+    );
+}
+
+pub fn app_main() {
+    println!("hello world");
+}
