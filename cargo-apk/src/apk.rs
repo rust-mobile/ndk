rust-mobile/ndk

commit e9a6ee1c7321335e5cbb6eb4e5e9045531fe43e2

cargo-apk: Normalize Windows UNC paths with dunce
Subcommand::target_dir() returns canonicalized paths through
sys:fs::canonicalize which contains a UNC prefix. This is unsupported by
most tools and results in a failed build:

'\\?\D:\Rust\android-ndk-rs\target\debug\apk\examples'
CMD.EXE was started with the above path as the current directory.
UNC paths are not supported.  Defaulting to Windows directory.
The system cannot find the path specified.

cargo-apk/src/apk.rs
@@ -31,7 +31,9 @@ impl<'a> ApkBuilder<'a> {
                 vec![ndk.detect_abi().unwrap_or(Target::Arm64V8a)]
             }
         };
-        let build_dir = cmd.target_dir().join(cmd.profile()).join("apk");
+        let build_dir = dunce::canonicalize(cmd.target_dir())?
+            .join(cmd.profile())
+            .join("apk");
         Ok(Self {
             cmd,
             ndk,
@@ -64,9 +66,7 @@ impl<'a> ApkBuilder<'a> {
 
         for target in &self.build_targets {
             let triple = target.rust_triple();
-            let build_dir = self
-                .cmd
-                .target_dir()
+            let build_dir = dunce::canonicalize(self.cmd.target_dir())?
                 .join(target.rust_triple())
                 .join(self.cmd.profile());
             let artifact = build_dir

cargo-apk/Cargo.toml
@@ -12,6 +12,7 @@ repository = "https://github.com/rust-windowing/android-ndk-rs"
 
 [dependencies]
 cargo-subcommand = "0.4.4"
+dunce = "1.0"
 env_logger = "0.7.1"
 exitfailure = "0.5.1"
 log = "0.4.8"

commit e86fde66dac4ec8f4a0801c80d9261e06672ff87

fixup: cargo-apk: Do not canonicalize target_dir(), only strip UNC
My previous commit introduced a canonicalize() here because target_dir()
returns a UNC path that is not supported by most tools. Needing the ?
operator should have rang a bell: dunce::canonicalize() calls
fs::canonicalize() first before stripping the UNC path. It bails if the
path doesn't exist, which happens in a clean environment when ./target
doesn't exist yet.

Luckily dunce also has a simplified() function to strip the UNC part off
the path without canonicalizing first.

Fixes: e9a6ee1c7321335e5cbb6eb4e5e9045531fe43e2

cargo-apk/src/apk.rs
@@ -31,7 +31,7 @@ impl<'a> ApkBuilder<'a> {
                 vec![ndk.detect_abi().unwrap_or(Target::Arm64V8a)]
             }
         };
-        let build_dir = dunce::canonicalize(cmd.target_dir())?
+        let build_dir = dunce::simplified(cmd.target_dir())
             .join(cmd.profile())
             .join("apk");
         Ok(Self {
@@ -66,7 +66,7 @@ impl<'a> ApkBuilder<'a> {
 
         for target in &self.build_targets {
             let triple = target.rust_triple();
-            let build_dir = dunce::canonicalize(self.cmd.target_dir())?
+            let build_dir = dunce::simplified(self.cmd.target_dir())
                 .join(target.rust_triple())
                 .join(self.cmd.profile());
             let artifact = build_dir