12402f67c74db9e49e232423eb2f40da5ca70951
JasperDeSutter <jasper.desutter@gmail.com>
2020-08-07 14:48:28 +0200

    cargo-apk: derive error using thiserror

--- cargo-apk/src/error.rs
+++ cargo-apk/src/error.rs
@@ -1,56 +1,23 @@
 use cargo_subcommand::Error as SubcommandError;
 use ndk_build::error::NdkError;
-use std::fmt::{Display, Formatter, Result};
 use std::io::Error as IoError;
+use thiserror::Error;
 use toml::de::Error as TomlError;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    Subcommand(SubcommandError),
-    Config(TomlError),
-    Ndk(NdkError),
-    Io(IoError),
+    #[error(transparent)]
+    Subcommand(#[from] SubcommandError),
+    #[error("Failed to parse config.")]
+    Config(#[from] TomlError),
+    #[error(transparent)]
+    Ndk(#[from] NdkError),
+    #[error(transparent)]
+    Io(#[from] IoError),
 }
 
 impl Error {
     pub fn invalid_args() -> Self {
         Self::Subcommand(SubcommandError::InvalidArgs)
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        match self {
-            Self::Subcommand(error) => error.fmt(f),
-            Self::Config(error) => return write!(f, "Failed to parse config: {}.", error),
-            Self::Ndk(error) => error.fmt(f),
-            Self::Io(error) => error.fmt(f),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
-
-impl From<SubcommandError> for Error {
-    fn from(error: SubcommandError) -> Self {
-        Self::Subcommand(error)
-    }
-}
-
-impl From<TomlError> for Error {
-    fn from(error: TomlError) -> Self {
-        Self::Config(error)
-    }
-}
-
-impl From<NdkError> for Error {
-    fn from(error: NdkError) -> Self {
-        Self::Ndk(error)
-    }
-}
-
-impl From<IoError> for Error {
-    fn from(error: IoError) -> Self {
-        Self::Io(error)
     }
 }
