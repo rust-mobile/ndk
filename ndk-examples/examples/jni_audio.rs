382b258da98560525e34dab00c9cfe192717d878
msiglreith <m.siglreith@gmail.com>
2020-09-23 18:36:53 +0200

    Add an JNI example (#73)
    * Add an JNI example
    
    * Apply suggestions from code review
    
    Improve example documentation
    
    Co-authored-by: Francesca Lovebloom <francesca@brainiumstudios.com>
    
    Co-authored-by: Francesca Lovebloom <francesca@brainiumstudios.com>

---
+++ ndk-examples/examples/jni_audio.rs
@@ -0,0 +1,73 @@
+#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+fn main() {
+    enumerate_audio_devices().unwrap();
+}
+
+const GET_DEVICES_OUTPUTS: jni::sys::jint = 2;
+
+fn enumerate_audio_devices() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a VM for executing Java calls
+    let native_activity = ndk_glue::native_activity();
+    let vm_ptr = native_activity.vm();
+    let vm = unsafe { jni::JavaVM::from_raw(vm_ptr) }?;
+    let env = vm.attach_current_thread()?;
+
+    // Query the global Audio Service
+    let class_ctxt = env.find_class("android/content/Context")?;
+    let audio_service = env.get_static_field(class_ctxt, "AUDIO_SERVICE", "Ljava/lang/String;")?;
+
+    let audio_manager = env
+        .call_method(
+            native_activity.activity(),
+            "getSystemService",
+            // JNI type signature needs to be derived from the Java API
+            // (ArgTys)ResultTy
+            "(Ljava/lang/String;)Ljava/lang/Object;",
+            &[audio_service],
+        )?
+        .l()?;
+
+    // Enumerate output devices
+    let devices = env.call_method(
+        audio_manager,
+        "getDevices",
+        "(I)[Landroid/media/AudioDeviceInfo;",
+        &[GET_DEVICES_OUTPUTS.into()],
+    )?;
+
+    println!("-- Output Audio Devices --");
+
+    let device_array = devices.l()?.into_inner();
+    let len = env.get_array_length(device_array)?;
+    for i in 0..len {
+        let device = env.get_object_array_element(device_array, i)?;
+
+        // Collect device information
+        // See https://developer.android.com/reference/android/media/AudioDeviceInfo
+        let product_name: String = {
+            let name =
+                env.call_method(device, "getProductName", "()Ljava/lang/CharSequence;", &[])?;
+            let name = env.call_method(name.l()?, "toString", "()Ljava/lang/String;", &[])?;
+            env.get_string(name.l()?.into())?.into()
+        };
+        let id = env.call_method(device, "getId", "()I", &[])?.i()?;
+        let ty = env.call_method(device, "getType", "()I", &[])?.i()?;
+
+        let sample_rates = {
+            let sample_array = env
+                .call_method(device, "getSampleRates", "()[I", &[])?
+                .l()?
+                .into_inner();
+            let len = env.get_array_length(sample_array)?;
+
+            let mut sample_rates = vec![0; len as usize];
+            env.get_int_array_region(sample_array, 0, &mut sample_rates)?;
+            sample_rates
+        };
+
+        println!("Device {}: Id {}, Type {}", product_name, id, ty);
+        println!("sample rates: {:#?}", sample_rates);
+    }
+
+    Ok(())
+}
