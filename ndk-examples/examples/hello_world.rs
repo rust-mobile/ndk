4a1b6cbe4cb56d7517a4af77e61871643f3263c7
Marijn Suijten <marijn@traverseresearch.nl>
2020-11-03 13:37:59 +0100

    ndk-examples: Use logger feature to print "hello world"
    It is more common to use a logging framework instead of printing raw
    text to stdout that ends up in the logs; lead by example.
    
    More importantly this exemplifies a current issue with android_logger
    not being exported by the relevant macro crate.

--- ndk-examples/examples/hello_world.rs
+++ ndk-examples/examples/hello_world.rs
@@ -1,10 +1,14 @@
+use log::info;
 use ndk::trace;
 
-#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+#[cfg_attr(
+    target_os = "android",
+    ndk_glue::main(backtrace = "on", logger(level = "debug", tag = "hello-world"))
+)]
 fn main() {
     let _trace;
     if trace::is_trace_enabled() {
         _trace = trace::Section::new("ndk-rs example main").unwrap();
     }
-    println!("hello world");
+    info!("hello world");
 }
