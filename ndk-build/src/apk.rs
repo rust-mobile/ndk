rust-mobile/ndk

commit 2dc644ceae77f00b8c07180116110d19b60b2416

Simplify some code in `ndk-macro` and `ndk-build` (#162)
* ndk-macro/helper: Use `as_deref()` instead of temporary in `map()`

* ndk-build: Deduplicate library path formatting in UnalignedApk::add_lib

ndk-build/src/apk.rs
@@ -79,17 +79,13 @@ impl<'a> UnalignedApk<'a> {
             return Err(NdkError::PathNotFound(path.into()));
         }
         let abi = target.android_abi();
-        let file_name = path.file_name().unwrap();
-        let out = self.0.build_dir.join("lib").join(abi);
-        std::fs::create_dir_all(&out)?;
-        std::fs::copy(path, out.join(&file_name))?;
+        let lib_path = Path::new("lib").join(abi).join(path.file_name().unwrap());
+        let out = self.0.build_dir.join(&lib_path);
+        std::fs::create_dir_all(out.parent().unwrap())?;
+        std::fs::copy(path, out)?;
 
         let mut aapt = self.0.build_tool(bin!("aapt"))?;
-        aapt.arg("add").arg(self.0.unaligned_apk()).arg(format!(
-            "lib/{}/{}",
-            abi,
-            file_name.to_str().unwrap()
-        ));
+        aapt.arg("add").arg(self.0.unaligned_apk()).arg(&lib_path);
         if !aapt.status()?.success() {
             return Err(NdkError::CmdFailed(aapt));
         }

ndk-macro/src/helper.rs
@@ -49,16 +49,11 @@ fn crate_name(name: &str) -> Result<String> {
 pub fn crate_path(name: &str, overridden_path: &Option<Path>) -> Path {
     // try to use overridden crate path
     overridden_path.clone().unwrap_or_else(|| {
-        // the binding to hold string from `crate_name` fn
-        let mut detected_name = None;
         Ident::new(
             // try to determine crate name from Cargo.toml
             crate_name(name)
                 .ok()
-                .map(|name| {
-                    detected_name = Some(name);
-                    detected_name.as_ref().unwrap().as_str()
-                })
+                .as_deref()
                 // or use default crate name
                 // (this may cause compilation error when crate is not found)
                 .unwrap_or_else(|| name),

commit 8de740b4ea02510ac9384f70d40aad357085168a

ndk-build: fix native activity library path on non-UNIX systems (#168)
Fixes: 2dc644c ("Simplify some code in `ndk-macro` and `ndk-build` (#162)")

cargo-apk/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "cargo-apk"
-version = "0.8.0"
+version = "0.8.1"
 authors = ["The Rust Windowing contributors"]
 edition = "2018"
 description = "Helps cargo build APKs"
@@ -16,7 +16,7 @@ cargo-subcommand = "0.5.0"
 dunce = "1.0.1"
 env_logger = "0.8.2"
 log = "0.4.14"
-ndk-build = { path = "../ndk-build", version = "0.4.0" }
+ndk-build = { path = "../ndk-build", version = "0.4.2" }
 serde = "1.0.123"
 thiserror = "1.0.23"
 toml = "0.5.8"

ndk-build/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "ndk-build"
-version = "0.4.1"
+version = "0.4.2"
 authors = ["The Rust Windowing contributors"]
 edition = "2018"
 description = "Utilities for building Android binaries"

ndk-build/src/apk.rs
@@ -84,8 +84,15 @@ impl<'a> UnalignedApk<'a> {
         std::fs::create_dir_all(out.parent().unwrap())?;
         std::fs::copy(path, out)?;
 
+        // Pass UNIX path separators to `aapt` on non-UNIX systems, ensuring the resulting separator
+        // is compatible with the target device instead of the host platform.
+        // Otherwise, it results in a runtime error when loading the NativeActivity `.so` library.
+        let lib_path_unix = lib_path.to_str().unwrap().replace("\\", "/");
+
         let mut aapt = self.0.build_tool(bin!("aapt"))?;
-        aapt.arg("add").arg(self.0.unaligned_apk()).arg(&lib_path);
+        aapt.arg("add")
+            .arg(self.0.unaligned_apk())
+            .arg(lib_path_unix);
         if !aapt.status()?.success() {
             return Err(NdkError::CmdFailed(aapt));
         }