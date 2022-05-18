cef7247e9fb55036f0cbdee69db90abb6a3bf956
Lattice 0 <lattice0@protonmail.com>
2022-05-18 19:58:15 -0300

    Update ndk/src/surface_texture.rs
    Co-authored-by: Marijn Suijten <marijns95@gmail.com>

--- ndk/src/surface_texture.rs
+++ ndk/src/surface_texture.rs
@@ -75,8 +75,8 @@ impl SurfaceTexture {
 
     /// # Safety
     ///
-    /// This function should be called by a healthy JVM pointer and with a non null surface texture,
-    /// which must be kept alive on the java/kotlin side.
+    /// This function should be called with a healthy JVM pointer and with a non-null surface texture,
+    /// which must be kept alive on the Java/Kotlin side.
     pub unsafe fn from_surface_texture(env: *mut JNIEnv, surface_texture: jobject) -> Option<Self> {
         let a_surface_texture_ptr = ffi::ASurfaceTexture_fromSurfaceTexture(env, surface_texture);
         let s = NonNull::new(a_surface_texture_ptr)?;
