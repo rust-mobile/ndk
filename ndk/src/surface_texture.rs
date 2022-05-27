8beef629966aadeaa805aaedd4c1aa6685ea348f
Lattice 0 <lattice0@protonmail.com>
2022-05-26 21:41:10 -0300

    Update ndk/src/surface_texture.rs
    Co-authored-by: Marijn Suijten <marijns95@gmail.com>

--- ndk/src/surface_texture.rs
+++ ndk/src/surface_texture.rs
@@ -90,7 +90,7 @@ impl SurfaceTexture {
         r
     }
 
-    /// Retrieve the timestamp associated with the texture image set by the most recent call to updateTexImage.
+    /// Retrieve the timestamp associated with the texture image set by the most recent call to [`update_tex_image()`][Self::update_tex_image()].
     pub fn timestamp(&self) -> i64 {
         unsafe { ffi::ASurfaceTexture_getTimestamp(self.ptr.as_ptr()) }
     }
