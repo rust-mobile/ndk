rust-mobile/ndk

commit 166ff3a35bf79d6a391913cc66f4f9d131367d79

Issue #34 #35 (#39)
* Issue #34 #35

* Issue #34 and #35 rustfmt

ndk-build/src/ndk.rs
@@ -14,17 +14,22 @@ pub struct Ndk {
 impl Ndk {
     pub fn from_env() -> Result<Self, NdkError> {
         let sdk_path = {
-            let mut sdk_path = std::env::var("ANDROID_SDK_HOME").ok();
-
+            let mut sdk_path = std::env::var("ANDROID_HOME").ok();
+            if sdk_path.is_some() {
+                println!(
+                    "Warning: You use environment variable ANDROID_HOME that is deprecated.\
+                 Please, remove it and use ANDROID_SDK_ROOT instead. Now ANDROID_HOME is used"
+                );
+            }
             if sdk_path.is_none() {
-                sdk_path = std::env::var("ANDROID_HOME").ok();
+                sdk_path = std::env::var("ANDROID_SDK_ROOT").ok();
             }
 
             PathBuf::from(sdk_path.ok_or(NdkError::SdkNotFound)?)
         };
 
         let ndk_path = {
-            let mut ndk_path = std::env::var("ANDROID_NDK_HOME").ok();
+            let mut ndk_path = std::env::var("ANDROID_NDK_ROOT").ok();
 
             if ndk_path.is_none() {
                 ndk_path = std::env::var("NDK_HOME").ok();

ndk-build/src/error.rs
@@ -24,12 +24,14 @@ impl Display for NdkError {
     fn fmt(&self, f: &mut Formatter) -> Result {
         let msg = match self {
             Self::SdkNotFound => {
-                "Please set the path to the Android SDK with either the $ANDROID_SDK_HOME or \
-                 the $ANDROID_HOME environment variable."
+                "Android SDK is not found. \
+                Please set the path to the Android SDK with the $ANDROID_SDK_ROOT \
+                environment variable."
             }
             Self::NdkNotFound => {
-                "Please set the path to the Android NDK with either the $ANDROID_NDK_HOME or \
-                 the $NDK_HOME environment variable."
+                "Android NDK is not found. \
+                Please set the path to the Android NDK with $ANDROID_NDK_ROOT \
+                environment variable."
             }
             Self::PathNotFound(path) => return write!(f, "Path {:?} doesn't exist.", path),
             Self::CmdNotFound(cmd) => return write!(f, "Command {} not found.", cmd),

commit fa88fd4ce53714515dd5dc86bc493fa27c8d4a03

ndk-build: Consider ANDROID_SDK_ROOT as deprecated instead of ANDROID_HOME
According to the [official documentation] `ndk-build` is considering the
wrong variable as deprecated.  The wrong message was introduced in [#39]
which bizarrely failed to quote any reference matterial on the matter,
making it impossible to understand whether this has always been the case
or recently switched in precedence.

[official documentation]: https://developer.android.com/studio/command-line/variables
[#39]: https://github.com/rust-windowing/android-ndk-rs/pull/39

ndk-build/src/ndk.rs
@@ -16,18 +16,20 @@ pub struct Ndk {
 impl Ndk {
     pub fn from_env() -> Result<Self, NdkError> {
         let sdk_path = {
-            let mut sdk_path = std::env::var("ANDROID_HOME").ok();
+            let sdk_path = std::env::var("ANDROID_SDK_ROOT").ok();
             if sdk_path.is_some() {
-                println!(
-                    "Warning: You use environment variable ANDROID_HOME that is deprecated. \
-                 Please, remove it and use ANDROID_SDK_ROOT instead. Now ANDROID_HOME is used"
+                eprintln!(
+                    "Warning: Environment variable ANDROID_SDK_ROOT is deprecated \
+                    (https://developer.android.com/studio/command-line/variables#envar). \
+                    It will be used until it is unset and replaced by ANDROID_HOME."
                 );
             }
-            if sdk_path.is_none() {
-                sdk_path = std::env::var("ANDROID_SDK_ROOT").ok();
-            }
 
-            PathBuf::from(sdk_path.ok_or(NdkError::SdkNotFound)?)
+            PathBuf::from(
+                sdk_path
+                    .or_else(|| std::env::var("ANDROID_HOME").ok())
+                    .ok_or(NdkError::SdkNotFound)?,
+            )
         };
 
         let ndk_path = {